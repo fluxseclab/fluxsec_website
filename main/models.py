faeeb598d9f619cd4a0f94cf2b76e793a61870e7
Mehdi Mohseni <wuka.sec@gmail.com>
2025-09-07 17:04:41 -0400

    News Detail (add title for render in news)

--- main/models.py
+++ main/models.py
@@ -3,10 +3,10 @@ from django.db import models
 
 class News(models.Model):
     url = models.CharField(max_length=200)
+    title = models.CharField(max_length=250, default='تایتیل وارد نشده است !')
     content = models.TextField()
     date_added = models.DateField(auto_now_add=True)
     is_translated = models.BooleanField(default=False)
-    
     
     def __str__(self):
         return self.content[:250]
